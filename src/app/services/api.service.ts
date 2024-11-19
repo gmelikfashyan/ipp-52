4f3aeef78288eb931c1099e58bd6526f370e8a76
eatthecookies <Aleksander.Vorobjev1@yandex.ru>
2024-11-19 23:20:53 +0300

    localhost

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:3000/api';
+  private baseUrl = 'http://89.208.105.80:3000/api';
 
   constructor(private http: HttpClient) {}
 
